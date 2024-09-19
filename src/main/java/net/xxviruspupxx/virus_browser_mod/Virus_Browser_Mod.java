4a90ec17f31f875e110ff3232daf2935807127a5
xXVirusPupXx <DarkSharkCaptain@gmail.com>
2024-09-19 07:20:03 -0500

    Project upload

---
+++ src/main/java/net/xxviruspupxx/virus_browser_mod/Virus_Browser_Mod.java
@@ -0,0 +1,13 @@
+package net.xxviruspupxx.virus_browser_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Virus_Browser_Mod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
